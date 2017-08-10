c5d6401fe72031892386b5df7183528d36f5d174
wurongjian <wurongjian@freshfirst.cn>
2017-08-10 15:44:30 +0800

    first commit

---
+++ src/vince/jxc/Services/TestService.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lzh
+ * Date: 17-8-10
+ * Time: 下午3:40
+ */
+
+namespace Vince\Jxc\Service;
+class TestService
+{
+        public function main()
+        {
+            dd('die .......^^_^^ ^^~^^');
+        }
+
+}
